80b87f5f0adfb5c128297cf1d22a2b28803f32d3
Luís Gomes <luis252001@hotmail.com>
2024-04-26 14:02:28 -0300

    chore: convert files to typescript

--- order-service/src/routes/router.ts
+++ order-service/src/routes/router.ts
@@ -1,5 +1,6 @@
 import { hamburguerRouter } from "./hamburguerRouter.js";
+import { Express } from "express";
 
-export const setupRoutes = (app) => {
+export const setupRoutes = (app: Express) => {
   app.use("/hamburguer", hamburguerRouter);
 };
