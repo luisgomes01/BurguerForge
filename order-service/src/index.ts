fe7c45a6aad6c9ad23bd8878742c4b62136132f6
Luís Gomes <luis252001@hotmail.com>
2024-04-29 14:46:03 -0300

    refactor: wrap index.ts in a startServer funciton

--- order-service/src/index.ts
+++ order-service/src/index.ts
@@ -4,20 +4,24 @@ import { connectRabbitMq } from "./services/rabbitmq.js";
 import { Channel, Connection } from "amqplib";
 import { connectMongoDb } from "./services/mongodb.js";
 
-const app = express();
-app.use(express.json());
+const startServer = () => {
+  const app = express();
+  app.use(express.json());
+  
+  const PORT = 3000;
+  
+  let connection!: Connection, channel!: Channel
+  
+  connectMongoDb()
+  
+  connectRabbitMq(connection, channel)
+  
+  setupRoutes(app);
+  
+  app.listen(PORT, () =>
+    console.log(`Order Service up and running on port ${PORT}`)
+  );
+}
 
-const PORT = 3000;
-
-let connection!: Connection, channel!: Channel
-
-connectMongoDb()
-
-connectRabbitMq(connection, channel)
-
-setupRoutes(app);
-
-app.listen(PORT, () =>
-  console.log(`Order Service up and running on port ${PORT}`)
-);
+startServer()
 
