35066dfe530b13461a23340e23b577e822b1bf4c
Luís Gomes <luis252001@hotmail.com>
2024-05-02 14:04:32 -0300

    chore: linting

--- order-service/src/index.ts
+++ order-service/src/index.ts
@@ -5,23 +5,20 @@ import { connectMongoDb } from "./services/mongodb.js";
 const startServer = () => {
   const app = express();
   app.use(express.json());
-  
-  const PORT = process.env.SERVER_PORT || 3000;
-  
 
-  connectMongoDb()
-  
-  
+  const PORT = process.env.SERVER_PORT || 3000;
+
+  connectMongoDb();
+
   setupRoutes(app);
-  
+
   app.listen(PORT, () =>
     console.log(`Order Service up and running on port ${PORT}`)
   );
-}
+};
 
-const waitForIt = Number(process.env.SLEEP_TIME) || 30000
+const waitForIt = Number(process.env.SLEEP_TIME) || 30000;
 
 setTimeout(() => {
-  startServer()
-}, waitForIt)
-
+  startServer();
+}, waitForIt);
--- order-service/src/services/mongodb.ts
+++ order-service/src/services/mongodb.ts
@@ -1,15 +1,14 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
-const port = process.env.MONGO_PORT || '27017'
-const host = process.env.MONGO_HOST || '127.0.0.1'
-const connectionUrl = `mongodb://${host}:${port}/order_service_db`
+const port = process.env.MONGO_PORT || "27017";
+const host = process.env.MONGO_HOST || "127.0.0.1";
+const connectionUrl = `mongodb://${host}:${port}/order_service_db`;
 
 export const connectMongoDb = async () => {
-    try {
-       await mongoose.connect(connectionUrl)
-       console.log('✅ Connected to MongoDb')
-    }
-    catch (error) {
-        console.log(`❌ Failed to connect to MongoDb: ${error}`)
-    }
-}
+  try {
+    await mongoose.connect(connectionUrl);
+    console.log("✅ Connected to MongoDb");
+  } catch (error) {
+    console.log(`❌ Failed to connect to MongoDb: ${error}`);
+  }
+};
