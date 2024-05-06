2ffed7fbfbfb4bff2dcdca6a8598308f7f7a8869
Luís Gomes <luis252001@hotmail.com>
2024-05-06 10:49:44 -0300

    feat: setup mongodb service

--- consumer-service/src/index.ts
+++ consumer-service/src/index.ts
@@ -1,10 +1,13 @@
 import express from "express";
+import { connectMongoDb } from "./services/mongodb.js";
 
 const startServer = () => {
   const app = express();
   app.use(express.json());
 
   const PORT = process.env.SERVER_PORT || 3001;
+
+  connectMongoDb();
 
   app.listen(PORT, () =>
     console.log(`Consumer Service up and running on port ${PORT}`)
